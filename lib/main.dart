73b4e2dd9d8d39693b8209c29783ad4d846d057e
Fernando-Braulio <44295487+Fernando-Braulio@users.noreply.github.com>
2019-10-19 15:21:14 -0300

    storing the api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const request = "https://api.hgbrasil.com/finance?format=json-cors&key=997fd35e";
+
 void main (){
   runApp(MaterialApp(
     home: Container(),
