ae4524893e3162671908ead08e55c8e2a1dff96f
Fernando-Braulio <44295487+Fernando-Braulio@users.noreply.github.com>
2019-10-19 16:11:12 -0300

    create textFileds

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,13 @@ const request = "https://api.hgbrasil.com/finance?format=json-cors&key=997fd35e"
 void main () async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+      ),
+    )
   ));
 }
 
@@ -22,6 +29,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,21 +65,42 @@ class _HomeState extends State<Home> {
                       textAlign: TextAlign.center,)
                 );
               }else{
-                return Container(color: Colors.green,);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Reais",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$ "
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Reais",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$ "
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                );
               }
           }
         }),
     );
   }
 }
-
-
-
-
-
-
-//theme: ThemeData(
-//inputDecorationTheme: InputDecorationTheme(
-//enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
-//)
-//)
