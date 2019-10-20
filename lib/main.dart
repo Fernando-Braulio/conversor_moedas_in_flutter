a0f9d798a5849949867c83f1537b4bfb0257b590
Fernando-Braulio <44295487+Fernando-Braulio@users.noreply.github.com>
2019-10-20 10:34:25 -0300

    0 clean function and decimal keyboard for ios

--- lib/main.dart
+++ lib/main.dart
@@ -38,21 +38,42 @@ class _HomeState extends State<Home> {
   double euro;
 
   void _realChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double real = double.parse(text);
     dolarController.text = (real/dolar).toStringAsFixed(2);
     euroController.text = (real/euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double dolar = double.parse(text);
     realController.text = (dolar * this.dolar).toStringAsFixed(2);
     euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double euro = double.parse(text);
     realController.text = (euro * this.euro).toStringAsFixed(2);
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
   }
 
   @override
@@ -123,6 +144,6 @@ Widget buildTextField(String label, String prefix, TextEditingController control
         color: Colors.amber,
         fontSize: 25.0),
     onChanged: fun,
-    keyboardType: TextInputType.number,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
   );
 }
